25039997a305445e1eea6a4feccfdda202c13d5a
Wanderleyps <wanderleyps794@gmail.com>
2023-09-10 00:04:57 -0300

    Initial commit

---
+++ IntroducaoPython/modulos/calculadora.py
@@ -0,0 +1,17 @@
+# Função de adição
+def somar(a, b):
+    return a + b
+
+# Função de subtração
+def subtrair(a, b):
+    return a - b
+
+# Função de multiplicação
+def multiplicar(a, b):
+    return a * b
+
+# Função de divisão
+def dividir(a, b):
+    if b == 0:
+        return "Erro: Divisão por zero não é permitida."
+    return a / b
---
+++ IntroducaoPython/modulos/main.py
@@ -0,0 +1,12 @@
+# Importe as funções do módulo calculadora
+from calculadora import somar, subtrair, multiplicar, dividir
+
+resultado_soma = somar(5, 3)
+resultado_subtracao = subtrair(10, 4)
+resultado_multiplicacao = multiplicar(6, 7)
+resultado_divisao = dividir(8, 2)
+
+print("Soma:", resultado_soma)
+print("Subtração:", resultado_subtracao)
+print("Multiplicação:", resultado_multiplicacao)
+print("Divisão:", resultado_divisao)
