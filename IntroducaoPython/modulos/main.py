364dea4f83b8924fc77afb9b85a3cc04a5c2faa0
Wanderleyps <wanderleyps794@gmail.com>
2023-09-13 00:08:50 -0300

    mostrando diferentes formas de realizar imports

--- IntroducaoPython/modulos/main.py
+++ IntroducaoPython/modulos/main.py
@@ -1,10 +1,12 @@
-# Importe as funções do módulo calculadora
-from calculadora import somar, subtrair, multiplicar, dividir
+# Importe algumas funções do módulo calculadora
+# from calculadora import somar, subtrair, multiplicar
 
-resultado_soma = somar(5, 3)
-resultado_subtracao = subtrair(10, 4)
-resultado_multiplicacao = multiplicar(6, 7)
-resultado_divisao = dividir(8, 2)
+import calculadora as calc
+
+resultado_soma = calc.somar(5, 3)
+resultado_subtracao = calc.subtrair(10, 4)
+resultado_multiplicacao = calc.multiplicar(6, 7)
+resultado_divisao = calc.dividir(8, 2)
 
 print("Soma:", resultado_soma)
 print("Subtração:", resultado_subtracao)
